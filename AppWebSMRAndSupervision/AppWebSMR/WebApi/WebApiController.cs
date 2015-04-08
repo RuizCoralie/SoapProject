003997c50b26fd7477f77538dd28b1e10ffd3159
Ruiz Coralie <ruiz.coralie64@live.fr>
2015-04-08 15:12:54 +0200

    Update LocationModels and controller

---
+++ AppWebSMRAndSupervision/AppWebSMR/WebApi/WebApiController.cs
@@ -0,0 +1,23 @@
+﻿using AppWebSMR.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net;
+
+namespace AppWebSMR.WebApi
+{
+    public class WebApiController
+    {
+        private static string _UrlWebAPi = System.Configuration.ConfigurationManager.AppSettings["ApiWeb"];
+
+        public static IEnumerable<LocationModels> GetLocations()
+        {
+            var locations = new List<LocationModels>();
+            WebClient wc = new WebClient();
+            string json = wc.DownloadString(string.Concat(_UrlWebAPi, "/api/Location"));
+
+            locations = JsonConvert.DeserializeObject<List<LocationModels>>(json);
+
+            return locations;
+        }
+    }
+}

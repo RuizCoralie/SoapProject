4648e8fe8d8dcbe99e9da230ad1e9649d2d07056
Ruiz Coralie <ruiz.coralie64@live.fr>
2015-04-09 17:26:33 +0200

    Send Location in AppMobile

--- AppWebSMRAndSupervision/AppWebSMR/Controllers/HomeController.cs
+++ AppWebSMRAndSupervision/AppWebSMR/Controllers/HomeController.cs
@@ -1,19 +1,21 @@
-﻿using System.Web.Mvc;
+﻿using AppWebSMR.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
 
 namespace AppWebSMR.Controllers
 {
     [Authorize]
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(IEnumerable<LocationModels> locations = null)
         {
-            return View();
+            return View(locations);
         }
 
         public ActionResult Refresh()
         {
             var locations = WebApi.WebApiController.GetLocations();
-            return View("Index");
+            return View("Index", locations);
         }
     }
 }
